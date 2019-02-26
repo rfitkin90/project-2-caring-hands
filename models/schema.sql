fe9a6cafb2188ca40bf6d01c6392019895c7d610
Richard Fitkin <rfitkin90@gmail.com>
2019-02-25 21:52:51 -0800

    added tables to schema

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,42 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+firstName VARCHAR(40) NOT NULL,
+lastName VARCHAR(40) NOT NULL,
+email VARCHAR(40) NOT NULL,
+password VARCHAR(40) NOT NULL,
+administrator BOOLEAN NOT NULL
+);
+
+DROP TABLE IF EXISTS residents;
+CREATE TABLE residents (
+firstName VARCHAR(40) NOT NULL,
+age INT,
+additionalnfo TEXT,
+activityPreferences TEXT,
+visitsSchedule
+);
+
+DROP TABLE IF EXISTS request;
+CREATE TABLE requests (
+availabilityStart
+availabilityEnd
+activities
+additionalInfo
+communityServiceFormRequest
+usersId INT NOT NULL,
+);
+
+DROP TABLE IF EXISTS scheduledVisits;
+CREATE TABLE requests (
+visitStart
+visitEnd
+activities
+additionalInfo
+communityServiceFormRequest
+usersId INT NOT NULL,
+residentsId INT NOT NULL
+);
