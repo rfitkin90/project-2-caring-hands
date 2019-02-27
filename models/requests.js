c7924a2b212226fdb88b00080654baf781b82ab4
Farjana Huq <farjanahuq@dhcp-v115-145.mobile.uci.edu>
2019-02-26 18:03:11 -0800

    Api-routes changed

--- models/requests.js
+++ models/requests.js
@@ -8,36 +8,37 @@
 //    usersId INT NOT NULL,
 // );
 
-module.exports = function (sequelize, DataTypes) {
-   var Residents = sequelize.define("Residents", {
-      // Giving the Author model a name of type STRING
-      availabilityStart: {
-         type: DataTypes.TIME,
-         allowNull: false
-      },
-      availabilityEnd: {
-         type: DataTypes.TIME,
-         allowNull: false
-      },
-      activityPreferences: {
-         type: DataTypes.STRING
-      },
-      additionalInfo: {
-         type: DataTypes.STRING
-      },
-      communityServiceForm: {
-         type: DataTypes.BOOLEAN,
+var Requests = sequelize.define("Requests", {
+   availabilityStart: {
+      type: DataTypes.TIME,
+      allowNull: false
+   },
+   availabilityEnd: {
+      type: DataTypes.TIME,
+      allowNull: false
+   },
+   visitDuration: {
+      type: DataTypes.INT,
+      defaultValue: 60
+   },
+   activityPreferences: {
+      type: DataTypes.STRING
+   },
+   additionalInfo: {
+      type: DataTypes.STRING
+   },
+   communityServiceForm: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+   }
+});
+
+Requests.associate = function (models) {
+   // Associating Author with Posts
+   // When an Author is deleted, also delete any associated Posts
+   Requests.belongsTo(models.UserModel, {
+      foreignKey: {
          allowNull: false
       }
    });
-
-   Residents.associate = function (models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
-      Residents.hasMany(models.Visits, {
-         onDelete: "cascade"
-      });
-   };
-
-   return Residents;
 };
