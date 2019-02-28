a9844747e48c3d9659a9df26cd4301847e4ec48f
Mika Diffee <mikadiffee@gmail.com>
2019-02-27 21:24:19 -0800

    updated resident page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -39,3 +39,24 @@ module.exports = function(app) {
     res.render("404");
   });
 };
+
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/resident.html"));
+  });
+
+  app.get("/cms", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  });
+
+  // blog route loads blog.html
+  app.get("/blog", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/resident.html"));
+  });
+
+};
+
