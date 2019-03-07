3bf056340039c0ada7a165d3dd23dfda5323556d
Richard Fitkin <rfitkin90@gmail.com>
2019-03-06 22:08:52 -0800

    added send grid

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 var model = require("../models");
 var express = require("express");
+var mandrill = require('node-mandrill')('c6NAwDNPJC0IQo-EJz9caA');
 var router = express.Router();
+var sgMail = require('@sendgrid/mail');
 
 
 // get id of user currently logged in
@@ -159,6 +161,25 @@ router.get('/visitsResidents/:id', function (req, res) {
     });
   ;
 });
+
+router.post('/sendemail/', function (req, res) {
+  console.log('send email post route hit');
+
+  // using SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'rfitkin90@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
+  res.end();
+  // or add res.json();
+})
+
 
 module.exports = router;
 
