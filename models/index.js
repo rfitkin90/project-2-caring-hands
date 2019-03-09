aa2b0c130fe897f70f4154ef8a9f1365a2dacbe5
Richard Fitkin <rfitkin90@gmail.com>
2019-03-09 10:02:52 -0800

    fix env variable

--- models/index.js
+++ models/index.js
@@ -13,10 +13,10 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    {
+      dialect: 'mysql',
+      use_env_variable: "JAWSDB_URL"
+    }
   );
 }
 
