570d9cb6ce262408e9fa5a8f1752741cc38778aa
rfitkin90 <rfitkin90@gmail.com>
2019-03-09 00:55:38 -0800

    added environment checking for email confirmation links

--- routes/apiRoutesEmail.js
+++ routes/apiRoutesEmail.js
@@ -12,22 +12,40 @@ router.post('/sendemail/', function (req, res) {
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   console.log('sendgrid api key set');
-  const msg = {
-    to: req.body.userEmail,
-    from: 'noreply@caring-hands.com',
-    subject: 'Confirm Caring Hands Appointment',
-    text: 'caring-hands',
-    html: `
+
+  // check for environment to send proper links in email
+  if (process.env.NODE_ENV === "test") {
+    const msg = {
+      to: req.body.userEmail,
+      from: 'noreply@caring-hands.com',
+      subject: 'Confirm Caring Hands Appointment',
+      text: 'caring-hands',
+      html: `
       <p>You have been shceduled for an appointment to see ${req.body.residentName} from ${req.body.visitStart} to ${req.body.visitEnd} doing the following activity: ${req.body.activity}. If you requested a community service form, one will be provided to you at hte time of your appointment.</p>
       <p>To confirm your appointment, please click the following link:</p>
       <a href="http://localhost:3000/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=1">http://localhost:3000/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=1</a>
       <p>To decline or cancel your appointment at any time, you may click the following link:</p>
       <a href="http://localhost:3000/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=0">http://localhost:3000/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=0</a>
     `,
-  };
-  sgMail.send(msg);
+    };
+    sgMail.send(msg);
+  } else {
+    const msg = {
+      to: req.body.userEmail,
+      from: 'noreply@caring-hands.com',
+      subject: 'Confirm Caring Hands Appointment',
+      text: 'caring-hands',
+      html: `
+      <p>You have been shceduled for an appointment to see ${req.body.residentName} from ${req.body.visitStart} to ${req.body.visitEnd} doing the following activity: ${req.body.activity}. If you requested a community service form, one will be provided to you at hte time of your appointment.</p>
+      <p>To confirm your appointment, please click the following link:</p>
+      <a href="https://young-reaches-88967.herokuapp.com/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=1">https://young-reaches-88967.herokuapp.com/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=1</a>
+      <p>To decline or cancel your appointment at any time, you may click the following link:</p>
+      <a href="https://young-reaches-88967.herokuapp.com/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=0">https://young-reaches-88967.herokuapp.com/visitConfirmation.html?k=${req.body.emailConfirmKey}&c=0</a>
+    `,
+    };
+    sgMail.send(msg);
+  }
   res.end();
-
 });
 
 module.exports = router;
